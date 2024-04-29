d33278b9950bde228adfa07b6a412bd98113c82d
Aurita08 <aura.gonzalez@unah.hn>
2024-04-29 10:53:21 -0600

    primer commit

---
+++ src/test/java/lenguajes/hn/reposicion/ReposicionApplicationTests.java
@@ -0,0 +1,13 @@
+package lenguajes.hn.reposicion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReposicionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
